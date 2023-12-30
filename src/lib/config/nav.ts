a37f9df7cbce361e120bd7ed7f21838bf06f11b0
t18n <me@t18n.com>
2023-12-30 01:25:47 +0100

    update links

--- src/lib/config/nav.ts
+++ src/lib/config/nav.ts
@@ -3,22 +3,22 @@ const routes = [
 		href: '/',
 		label: 'Home'
 	},
-	{
-		href: '/projects',
-		label: 'Projects'
-	},
-	{
-		href: '/services',
-		label: 'Services'
-	},
+	// {
+	// 	href: '/projects',
+	// 	label: 'Projects'
+	// },
+	// {
+	// 	href: '/services',
+	// 	label: 'Services'
+	// },
 	{
 		href: '/about',
 		label: 'About Me'
 	},
-	// {
-	// 	href: 'https://wedding.t18n.com',
-	// 	label: 'My wedding'
-	// },
+	{
+		href: 'https://wedding.t18n.com',
+		label: 'My wedding'
+	},
 ];
 
 export default routes;
