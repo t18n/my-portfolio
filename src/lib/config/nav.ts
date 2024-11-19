f681449ed137c856c117346e74e9a80820240aa7
t18n <me@t18n.com>
2024-11-19 08:57:22 +0100

    fix: remove my wedding

--- src/lib/config/nav.ts
+++ src/lib/config/nav.ts
@@ -1,24 +1,24 @@
 const routes = [
-	{
-		href: '/',
-		label: 'Home'
-	},
-	// {
-	// 	href: '/projects',
-	// 	label: 'Projects'
-	// },
-	// {
-	// 	href: '/services',
-	// 	label: 'Services'
-	// },
-	{
-		href: '/about',
-		label: 'About Me'
-	},
-	{
-		href: 'https://wedding.t18n.com',
-		label: 'My wedding'
-	},
+  {
+    href: '/',
+    label: 'Home'
+  },
+  // {
+  // 	href: '/projects',
+  // 	label: 'Projects'
+  // },
+  // {
+  // 	href: '/services',
+  // 	label: 'Services'
+  // },
+  {
+    href: '/about',
+    label: 'About Me'
+  },
+  // {
+  // 	href: 'https://wedding.t18n.com',
+  // 	label: 'My wedding'
+  // },
 ];
 
 export default routes;
